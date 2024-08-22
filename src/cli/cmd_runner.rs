90221e3cc44c7a4718f6a75da10e67bef6a0462b
Travis Ennis <travis@travisennis.com>
2024-08-22 18:27:19 -0400

    docs: add code docs to cmd_runner.rs

--- src/cli/cmd_runner.rs
+++ src/cli/cmd_runner.rs
@@ -1,5 +1,34 @@
 use std::error::Error;
 
+/// A trait representing a command runner.
+///
+/// This trait defines a single method, `run`, which is responsible for executing the logic of a specific command.
+/// Implementations of this trait are responsible for handling command-specific arguments, interacting with
+/// the appropriate services or APIs, and performing the necessary actions based on the command's purpose.
+///
+/// # Example
+///
+/// ```rust
+/// use std::error::Error;
+///
+/// #[derive(Debug)]
+/// struct MyCommand;
+///
+/// impl CmdRunner for MyCommand {
+///     async fn run(&self) -> Result<(), Box<dyn Error + Send + Sync>> {
+///         println!("Running MyCommand!");
+///         Ok(())
+///     }
+/// }
+/// ```
 pub trait CmdRunner {
+    /// Executes the command's logic.
+    ///
+    /// This method is responsible for handling command-specific arguments, interacting with
+    /// the appropriate services or APIs, and performing the necessary actions based on the command's purpose.
+    ///
+    /// # Returns
+    ///
+    /// Returns a `Result` indicating whether the command execution was successful.
     async fn run(&self) -> Result<(), Box<dyn Error + Send + Sync>>;
 }
